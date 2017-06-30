2ca3c8b4dddd0d3d902ee98addbe4d42eea75a1d
James White <jawh@microsoft.com>
2017-06-30 16:28:56 -0700

    Uploading App Example

---
+++ FormsFABExample/FormsFABExample.Android/MyFloatingActionButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.Design.Widget;
+
+namespace FormsFABExample.Droid
+{
+    public class MyFloatingActionButton : FloatingActionButton
+    {
+        private bool isClicked = true;
+        public bool ToggleSwitch
+        {
+            get
+            {
+                return isClicked;
+            }
+            set
+            {
+                isClicked = value;
+            }
+        }
+
+        public MyFloatingActionButton(Context context) : base(context)
+        {
+            this.Click += OnButtonClicked;
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            //Some sort of event to trigger a change
+            ToggleSwitch = !isClicked;
+        }
+    }
+}
---
+++ FormsFABExample/FormsFABExample/DataViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsFABExample
+{
+    public class DataViewModel : INotifyPropertyChanged
+    {
+        private bool _toggleOnOff = true;
+        public bool OnAndOff
+        {
+            get
+            {
+                return _toggleOnOff;
+            }
+            set
+            {
+                _toggleOnOff = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            var handle = PropertyChanged;
+            handle?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
